915bcfd84152f0300ea1e39fcd31c80ed16890f1
cvishak <vishakh@eteaminc.com>
2022-09-07 23:09:54 +0530

    Codebase created

---
+++ direct-sourcing/src/component/job/joblist/Index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            Job List
+        </div>
+    );
+};
+
+export default Index;
---
+++ direct-sourcing/src/component/job/viewjob/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            View Job
+        </div>
+    );
+};
+
+export default index;
