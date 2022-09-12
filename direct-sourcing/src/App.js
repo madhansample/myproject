d647d38785b34ad756fb6fafc49e4372ca9c16f1
Gauri Zade <gaurizade@gmail.com>
2022-09-12 17:49:23 +0530

    viewJob page

--- direct-sourcing/src/App.js
+++ direct-sourcing/src/App.js
@@ -9,11 +9,13 @@ import {
 import JobList from "./component/job/joblist/Index";
 import Index from "./component/job/viewjob/Index";
 import ApplyJob from "./component/job/viewjob/ApplyJob";
+import ViewJob from './component/job/viewjob/Index'
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/' element={<ViewJob />} ></Route>
         <Route path="/" element={<JobList />}></Route>
         <Route path="/careers" element={<Index />}></Route>
         <Route path="/apply" element={<ApplyJob />}></Route>
