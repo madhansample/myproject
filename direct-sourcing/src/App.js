bb7a8b6f7f703e832de8bcb8ef4b2422ca9ed260
yogesh kumar <ykumar@eteaminc.com>
2022-09-12 14:14:02 +0530

    ApplyJobComponent

--- direct-sourcing/src/App.js
+++ direct-sourcing/src/App.js
@@ -1,17 +1,23 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Switch, Redirect } from 'react-router-dom';
-import JobList from './component/job/joblist/Index'
-import Index from './component/job/viewjob/Index';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import JobList from "./component/job/joblist/Index";
+import Index from "./component/job/viewjob/Index";
+import ApplyJob from "./component/job/viewjob/ApplyJob";
 
 function App() {
   return (
-    <Router>    
+    <Router>
       <Routes>
-        <Route path='/' element={<JobList />} >
-        </Route>    
-        <Route path='/careers' element={<Index />}>
-          </Route>    
-        </Routes>
+        <Route path="/" element={<JobList />}></Route>
+        <Route path="/careers" element={<Index />}></Route>
+        <Route path="/apply" element={<ApplyJob />}></Route>
+      </Routes>
     </Router>
   );
 }
