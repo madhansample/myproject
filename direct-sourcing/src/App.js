0e14dc42a6ea3e94ae85063fbf5ff3a059a48ae8
cvishak <vishakh@eteaminc.com>
2022-09-12 23:15:20 +0530

    Common route deleted

--- direct-sourcing/src/App.js
+++ direct-sourcing/src/App.js
@@ -1,6 +1,5 @@
 import { useRoutes } from "react-router-dom";
 import { ComponentList } from "./routes/ComponentList";
-import Index from "./component/job/viewjob/Index";
 
 const App = () => {
   let routes = useRoutes(ComponentList);
